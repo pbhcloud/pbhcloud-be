c4ac3cda4f800d81d0c5517b40103dbcf4bb4b6a
Shon Aizenshtein <68730456+Senior-ai@users.noreply.github.com>
2024-07-15 11:30:46 +0300

    minor changes

--- models/userModel.py
+++ models/userModel.py
@@ -12,7 +12,7 @@ class User(BaseModel):
     password: str
     rootFolder: str
     settings: list
-    creationDate: int= int(datetime.timestamp(datetime.now()))
+    # creationDate: int= int(datetime.timestamp(datetime.now()))
 
     def to_json(self):
         return json.dumps(self, execlude_none=True)
@@ -22,44 +22,41 @@ class User(BaseModel):
         if data.get("_id") is None:
             data.pop("_id", None)
         return data
-
-# Define the user schema
-# user_schema = {
-#     "name": {"type": str, "required": True},
-#     "email": {
-#         "type": str,
-#         "required": True,
-#         "unique": True,
-#         "lowercase": True,
-#         "validate": validator.is_email,
-#     },
-#     "picture": {"type": str, "default": "https://res.cloudinary.com/dkd5jblv5/image/upload/v1675976806/Default_ProfilePicture_gjngnb.png"},
-#     "password": {
-#         "type": str,
-#         "required": True,
-#         "min_length": 6,
-#         "max_length": 48,
-#     },
-#     "rootFolder": {"type": object, }, #TODO - Gotta verify later on what should be added here to define rootFolder a bit better.
-#     "settings": {"type": list, "default": [], "required": True}, #Currently, in document its written as obj although I think we can handle settings as a JSON list or something like that.
-# }
-
-# #TODO- Define a pre-save hook to hash the password
-
-# #TODO- Add the pre-save hook to the user schema
-# # user_schema["pre_save"] = hash_password
-
-# # Create the UserModel class
-# class UserModel:
-#     def __init__(self, **kwargs):
+    
+    #     def __init__(self, **kwargs):
 #         for field, value in kwargs.items():
 #             setattr(self, field, value)
 
 #     def save(self):
 #         users.insert_one(self.__dict__)
 
+# Define the user schema
+user_schema = {
+    "name": {"type": str, "required": True},
+    "email": {
+        "type": str,
+        "required": True,
+        "unique": True,
+        "lowercase": True,
+    },
+    "picture": {"type": str, "default": "https://res.cloudinary.com/dkd5jblv5/image/upload/v1675976806/Default_ProfilePicture_gjngnb.png"},
+    "password": {
+        "type": str,
+        "required": {True, 'Please provide your password'},
+        "min_length": {6, 'Please make sure your password is at least 6 characters long'},
+        "max_length": {48, 'Please make sure your password is less than 48 characters long'},
+    },
+    "rootFolder": {"type": object, }, #TODO - Gotta verify later on what should be added here to define rootFolder a bit better.
+    "settings": {"type": list, "default": [], "required": True}, #Currently, in document its written as obj although I think we can handle settings as a JSON list or something like that.
+}
+
+# #TODO- Define a pre-save hook to hash the password
+
+# #TODO- Add the pre-save hook to the user schema
+# # user_schema["pre_save"] = hash_password
+
 # # Instantiate the UserModel
-# UserModel = UserModel(**user_schema)
+UserModel = User(**user_schema)
 
 # # Example usage:
 # new_user = UserModel(name="John Doe", email="john@example.com", password="my_secure_password")
